5fca9962b4008bb68442cc2b5044616dda4097f3
amirmvahed <amirmvahed@gmail.com>
2023-07-11 12:52:19 +0330

    05-linking-and-navigating

--- app/page.js
+++ app/page.js
@@ -1,3 +1,22 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1>Welcome home!</h1>;
+  return <>
+    <h1>Welcome home!</h1>
+    <Link href="/users">Users</Link>
+  </>
 }
+
+// 'use client'
+//
+// import { useRouter } from 'next/navigation'
+//
+// export default function Home() {
+//   const router = useRouter()
+//   return <>
+//     <h1>Welcome home!</h1>
+//     <button type="button" onClick={() => router.push('/users')}>
+//       Users
+//     </button>
+//   </>
+// }
--- app/users/page.js
+++ app/users/page.js
@@ -1,10 +1,32 @@
+import Link from 'next/link'
+
+const users = [
+    {
+        id: '1',
+        name: 'amir'
+    },
+    {
+        id: '2',
+        name: 'ali'
+    },
+    {
+        id: '3',
+        name: 'maryam'
+    },
+    {
+        id: '4',
+        name: 'zahra'
+    },
+]
+
 export default function Users() {
     return (
-        <>
-            <h1>Users List</h1>
-            <h2>user 1</h2>
-            <h2>user 2</h2>
-            <h2>user 3</h2>
-        </>
-    );
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>
+                    <Link href={`/users/${user.id}`}>{user.name}</Link>
+                </li>
+            ))}
+        </ul>
+    )
 }
